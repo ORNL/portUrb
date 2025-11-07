aae38e3ed7ad914f2c7570c0440311aa3fcb5bb4
Matt Norman <normanmr@ornl.gov>
2025-11-07 10:20:23 -0500

    Changing turibne analysis file

--- build/postproc/analyze_turbine_les.py
+++ build/postproc/analyze_turbine_les.py
@@ -14,7 +14,10 @@ files_t = {f"{wind}_{time}": f"{workdir}/{prefix}_{z0:.6f}_wind-{wind:.6f}_turbi
 xmin = 2
 xmax = 0
 z = np.array(Dataset(files_n[f"{winds[0]}_{times[0]}"],"r")["z"][:])
-ratios = np.array([[0. for i in range(len(z))] for j in range(len(winds))])
+noturb_u = np.array([[0. for i in range(len(z))] for j in range(len(winds))])
+noturb_v = np.array([[0. for i in range(len(z))] for j in range(len(winds))])
+turb_u   = np.array([[0. for i in range(len(z))] for j in range(len(winds))])
+turb_v   = np.array([[0. for i in range(len(z))] for j in range(len(winds))])
 for iwind in range(len(winds)) :
     wind = winds[iwind]
     for itime in range(len(times)) :
@@ -22,41 +25,40 @@ for iwind in range(len(winds)) :
         nc    = Dataset(files_n[f"{wind}_{time}"],"r")
         u     = np.array(nc["avg_u"][:,:,:])
         v     = np.array(nc["avg_v"][:,:,:])
+        noturb_u[iwind,:] = np.mean(u,axis=(1,2)) if time==times[0] else noturb_u[iwind,:]+np.mean(u,axis=(1,2))
+        noturb_v[iwind,:] = np.mean(v,axis=(1,2)) if time==times[0] else noturb_v[iwind,:]+np.mean(v,axis=(1,2))
         mag   = np.sqrt(u*u+v*v)
         mag_n = mag if time==times[0] else mag_n+mag
 
         nc    = Dataset(files_t[f"{wind}_{time}"],"r")
         u     = np.array(nc["avg_u"][:,:,:])
         v     = np.array(nc["avg_v"][:,:,:])
+        turb_u[iwind,:] = np.mean(u,axis=(1,2)) if time==times[0] else turb_u[iwind,:]+np.mean(u,axis=(1,2))
+        turb_v[iwind,:] = np.mean(v,axis=(1,2)) if time==times[0] else turb_v[iwind,:]+np.mean(v,axis=(1,2))
         mag   = np.sqrt(u*u+v*v)
         mag_t = mag if time==times[0] else mag_t+mag
-    mag_n /= len(times)
-    mag_t /= len(times)
+    mag_n             /= len(times)
+    mag_t             /= len(times)
+    noturb_u[iwind,:] /= len(times)
+    noturb_v[iwind,:] /= len(times)
+    turb_u  [iwind,:] /= len(times)
+    turb_v  [iwind,:] /= len(times)
     x = np.array(nc["x"][:])
     y = np.array(nc["y"][:])
     z = np.array(nc["z"][:])
     xlen = x[-1]+(x[1]-x[0])/2
     ylen = y[-1]+(y[1]-y[0])/2
+    turb_u = noturb_u*(10000**2-xlen*ylen)/(10000**2) + turb_u*(xlen*ylen)/(10000**2)
+    turb_v = noturb_v*(10000**2-xlen*ylen)/(10000**2) + turb_v*(xlen*ylen)/(10000**2)
 
-    ratio_dom = np.mean(mag_t,axis=(1,2))/np.mean(mag_n,axis=(1,2))
-    ratio = 1*(10000**2-xlen*ylen)/(10000**2) + ratio_dom*(xlen*ylen)/(10000**2)
-    ratios[iwind,:] = ratio
-    # xmin = min(np.min(ratio),xmin)
-    # xmax = max(np.max(ratio),xmax)
-    # plt.plot(ratio,z,label=f"Inflow={wind}m/s")
-# plt.fill_between([xmin,xmax],90-63,90+63,color="lightskyblue")
-# plt.legend()
-# plt.xlabel(r"$\sqrt{u*u+v*v}$ no_turbine / turbine ratio")
-# plt.ylabel(r"Height (m)")
-# plt.grid()
-# plt.show()
-# plt.close()
-
-nc = Dataset(f"ratios_{z0:.6f}.nc","w",format='NETCDF4')
+nc = Dataset(f"turbine_coarse_z0_{z0:.6f}.nc","w",format='NETCDF4')
 nc.createDimension('z'   ,len(z    ))
 nc.createDimension('wind',len(winds))
-nc.createVariable('z'     ,'f4',('z'       ))[:]   = z     [:]
-nc.createVariable('wind'  ,'f4',('wind'    ))[:]   = winds [:]
-nc.createVariable('ratios','f4',('wind','z'))[:,:] = ratios[:,:]
+nc.createVariable('z'       ,'f4',('z'       ))[:]   = z       [:]
+nc.createVariable('wind'    ,'f4',('wind'    ))[:]   = winds   [:]
+nc.createVariable('noturb_u','f4',('wind','z'))[:,:] = noturb_u[:,:]
+nc.createVariable('noturb_v','f4',('wind','z'))[:,:] = noturb_v[:,:]
+nc.createVariable('turb_u'  ,'f4',('wind','z'))[:,:] = turb_u  [:,:]
+nc.createVariable('turb_v'  ,'f4',('wind','z'))[:,:] = turb_v  [:,:]
 nc.close()
 
