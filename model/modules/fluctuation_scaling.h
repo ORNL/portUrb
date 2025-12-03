2128f381959031f339e9a527520bbbd7bb9c5a2a
Matt Norman <normanmr@ornl.gov>
2025-12-03 15:29:41 -0500

    Documentation for fluctuation_scaling and geostrophic_wind_forcing

--- model/modules/fluctuation_scaling.h
+++ model/modules/fluctuation_scaling.h
@@ -5,6 +5,16 @@
 
 namespace modules {
   
+  // This function scales the fluctuations about the mean of the specified fields
+  //   by a factor determined by frac, dt, and tscale
+  // coupler : Coupler object to access domain and data information
+  // dt      : Timestep size
+  // frac    : Fractional reduction in fluctuations over the timescale tscale
+  // tscale  : Timescale over which fluctuations are reduced by the fraction frac
+  // vnames  : Vector of variable names to apply fluctuation scaling to
+  // This is typically used to modify turbulent precursor inflow to different turbulence intensities
+  // Typically you'll save the precursor data, then apply this routine to scale the turbulence,
+  //   and then restore the precursor data to its original values so that only the inflow is modified
   inline void fluctuation_scaling( core::Coupler            & coupler ,
                                    real dt                            ,
                                    real frac                          ,
@@ -12,23 +22,24 @@ namespace modules {
                                    std::vector<std::string>   vnames  ) {
     using yakl::c::parallel_for;
     using yakl::c::SimpleBounds;
-    using yakl::componentwise::operator/;
-    using yakl::componentwise::operator-;
-    auto nx       = coupler.get_nx();
-    auto ny       = coupler.get_ny();
-    auto nz       = coupler.get_nz();
-    auto i_beg    = coupler.get_i_beg();
-    auto j_beg    = coupler.get_j_beg();
-    auto nx_glob  = coupler.get_nx_glob();
-    auto ny_glob  = coupler.get_ny_glob();
-    auto &dm      = coupler.get_data_manager_readwrite();
+    using yakl::componentwise::operator/;  // Allows use of '/' on yakl::Array objects
+    using yakl::componentwise::operator-;  // Allows use of '-' on yakl::Array objects
+    auto nx       = coupler.get_nx();      // Get local number of cells in x-direction
+    auto ny       = coupler.get_ny();      // Get local number of cells in y-direction
+    auto nz       = coupler.get_nz();      // Get local number of cells in z-direction
+    auto i_beg    = coupler.get_i_beg();   // Get global starting index in x-direction
+    auto j_beg    = coupler.get_j_beg();   // Get global starting index in y-direction
+    auto nx_glob  = coupler.get_nx_glob(); // Get global number of cells in x-direction
+    auto ny_glob  = coupler.get_ny_glob(); // Get global number of cells in y-direction
+    auto &dm      = coupler.get_data_manager_readwrite(); // Get DataManager for read/write access
+    // Accure the specified fields into a MultiField of 3-D arrays
     core::MultiField<real,3> fields;
     int numvars = vnames.size();
     for (int i=0; i < numvars; i++) { fields.add_field( dm.get<real,3>(vnames.at(i)) ); }
 
-    real mult = std::pow(frac,dt/tscale);
+    real mult = std::pow(frac,dt/tscale);  // Pre-compute the fluctuation scaling multiplier
 
-    // Get the mean column for main values
+    // Compute the locally summed column values for each variable
     real2d col("main_col_mean",numvars,nz);
     parallel_for( YAKL_AUTO_LABEL() , SimpleBounds<2>(numvars,nz) , KOKKOS_LAMBDA (int l, int k) {
       col(l,k) = 0;
@@ -38,9 +49,9 @@ namespace modules {
         }
       }
     });
-
+    // Reduce the column sums across all MPI ranks and compute the global mean
     col = coupler.get_parallel_comm().all_reduce(col,MPI_SUM)/(nx_glob*ny_glob);
-
+    // Now scale the fluctuations about the mean for each variable
     parallel_for( YAKL_AUTO_LABEL() , SimpleBounds<4>(numvars,nz,ny,nx) , KOKKOS_LAMBDA (int l, int k, int j, int i) {
       fields(l,k,j,i) = col(l,k) + mult*(fields(l,k,j,i)-col(l,k));
     });
--- model/modules/geostrophic_wind_forcing.h
+++ model/modules/geostrophic_wind_forcing.h
@@ -5,24 +5,35 @@
 
 namespace modules {
 
+  // Apply geostrophic forcing to the velocity fields
+  // coupler : core::Coupler object containing the data manager and parallel comm info
+  // dt      : Timestep size in seconds
+  // lat_g   : Geostrophic latitude in degrees
+  // u_g     : Geostrophic zonal wind speed in m/s
+  // v_g     : Geostrophic meridional wind speed in m/s
+  // Returns a real2d array of shape (2,nz) containing the domain-averaged u and v velocities at each vertical
+  //   to be later used as specified forcing for simulations forced by turbulent precursor using the
+  //   geostrophic_wind_forcing_specified routine.
+  // This routine applyies geostrophic forcing to the overall model column rather than each individual cell.
   inline real2d geostrophic_wind_forcing( core::Coupler &coupler , real dt , real lat_g , real u_g , real v_g ) {
     using yakl::c::parallel_for;
     using yakl::c::SimpleBounds;
-    auto nz      = coupler.get_nz();
-    auto ny      = coupler.get_ny();
-    auto nx      = coupler.get_nx();
-    auto ny_glob = coupler.get_ny_glob();
-    auto nx_glob = coupler.get_nx_glob();
-    auto &dm     = coupler.get_data_manager_readwrite();
-    auto uvel    = dm.get<real,3>("uvel");
-    auto vvel    = dm.get<real,3>("vvel");
-    auto imm     = dm.get<real const,3>("immersed_proportion");
-    real fcor    = 2*7.2921e-5*std::sin(lat_g/180*M_PI);
-    int constexpr idU  = 0;
-    int constexpr idV  = 1;
-    int constexpr nfld = 2;
-    real2d col("col",nfld,nz);
-    real r_nx_ny = 1. / (ny_glob*nx_glob);
+    auto nz      = coupler.get_nz();       // number of vertical levels
+    auto ny      = coupler.get_ny();       // number of local cells in the y-direction
+    auto nx      = coupler.get_nx();       // number of local cells in the x-direction
+    auto ny_glob = coupler.get_ny_glob();  // total global number of cells in the y-direction
+    auto nx_glob = coupler.get_nx_glob();  // total global number of cells in the x-direction
+    auto &dm     = coupler.get_data_manager_readwrite();  // get the data manager with read/write access
+    auto uvel    = dm.get<real,3>("uvel");  // Get the 3D u-velocity field
+    auto vvel    = dm.get<real,3>("vvel");  // Get the 3D v-velocity field
+    auto imm     = dm.get<real const,3>("immersed_proportion"); // Get the immersed proportion field
+    real fcor    = 2*7.2921e-5*std::sin(lat_g/180*M_PI);  // Compute coriolis parameter
+    int constexpr idU  = 0;  // label for u-velocity in the column-averaged array
+    int constexpr idV  = 1;  // label for v-velocity in the column-averaged array
+    int constexpr nfld = 2;  // number of velocity fields (u and v)
+    real2d col("col",nfld,nz);  // Allocate averaged column array for u and v velocities
+    real r_nx_ny = 1. / (ny_glob*nx_glob); // precompute reciprocal of total global horizontal cells
+    // Compute local contributions to the column-averaged velocities
     parallel_for( YAKL_AUTO_LABEL() , SimpleBounds<2>(nfld,nz) , KOKKOS_LAMBDA (int v, int k) {
       col(v,k) = 0;
       for (int j=0; j < ny; j++) {
@@ -32,48 +43,70 @@ namespace modules {
         }
       }
     });
+    // Reduce across all MPI ranks to get the global column-averaged velocities
     col = coupler.get_parallel_comm().all_reduce( col , MPI_SUM , "" );
+    // Apply geostrophic forcing to the u and v velocity fields based on averaged column forcing
     parallel_for( YAKL_AUTO_LABEL() , SimpleBounds<3>(nz,ny,nx) , KOKKOS_LAMBDA (int k, int j, int i) {
       uvel(k,j,i) += dt*( fcor*(col(idV,k)-v_g));
       vvel(k,j,i) += dt*(-fcor*(col(idU,k)-u_g));
     });
-    return col;
+    return col; // Return the column-averaged velocities to be used by geostrophic_wind_forcing_specified
   }
 
+
+  // Re-use a specified average column to perform geostrophic forcing that was applied to a precursor simulation
+  // coupler : core::Coupler object containing the data manager and parallel comm info
+  // dt      : Timestep size in seconds
+  // lat_g   : Geostrophic latitude in degrees
+  // u_g     : Geostrophic zonal wind speed in m/s
+  // v_g     : Geostrophic meridional wind speed in m/s
+  // col     : real2d array of shape (2,nz) containing the domain-averaged u and v velocities at each vertical level
+  //           to be used as specified forcing
+  // This routine applies geostrophic forcing to the overall model column rather than each individual cell.
   inline void geostrophic_wind_forcing_specified( core::Coupler &coupler , real dt , real lat_g , real u_g , real v_g ,
                                                   real2d const &col ) {
     using yakl::c::parallel_for;
     using yakl::c::SimpleBounds;
-    auto nz      = coupler.get_nz();
-    auto ny      = coupler.get_ny();
-    auto nx      = coupler.get_nx();
-    auto ny_glob = coupler.get_ny_glob();
-    auto nx_glob = coupler.get_nx_glob();
-    auto &dm     = coupler.get_data_manager_readwrite();
-    auto uvel    = dm.get<real,3>("uvel");
-    auto vvel    = dm.get<real,3>("vvel");
-    real fcor    = 2*7.2921e-5*std::sin(lat_g/180*M_PI);
-    int constexpr idU  = 0;
-    int constexpr idV  = 1;
+    auto nz      = coupler.get_nz();       // number of vertical levels
+    auto ny      = coupler.get_ny();       // number of local cells in the y-direction
+    auto nx      = coupler.get_nx();       // number of local cells in the x-direction
+    auto ny_glob = coupler.get_ny_glob();  // total global number of cells in the y-direction
+    auto nx_glob = coupler.get_nx_glob();  // total global number of cells in the x-direction
+    auto &dm     = coupler.get_data_manager_readwrite();  // get the data manager with read/write access
+    auto uvel    = dm.get<real,3>("uvel");  // Get the 3D u-velocity field
+    auto vvel    = dm.get<real,3>("vvel");  // Get the 3D v-velocity field
+    real fcor    = 2*7.2921e-5*std::sin(lat_g/180*M_PI);  // Compute coriolis parameter
+    int constexpr idU  = 0;  // label for u-velocity in the column-averaged array
+    int constexpr idV  = 1;  // label for v-velocity in the column-averaged array
+    // Apply geostrophic forcing to the u and v velocity fields based on averaged column forcing
     parallel_for( YAKL_AUTO_LABEL() , SimpleBounds<3>(nz,ny,nx) , KOKKOS_LAMBDA (int k, int j, int i) {
       uvel(k,j,i) += dt*( fcor*(col(idV,k)-v_g));
       vvel(k,j,i) += dt*(-fcor*(col(idU,k)-u_g));
     });
   }
 
+
+  // Apply geostrophic forcing to the velocity fields for each individual cell
+  // coupler : core::Coupler object containing the data manager and parallel comm info
+  // dt      : Timestep size in seconds
+  // lat_g   : Geostrophic latitude in degrees
+  // u_g     : Geostrophic zonal wind speed in m/s
+  // v_g     : Geostrophic meridional wind speed in m/s
+  // This routine applies geostrophic forcing to each individual cell rather than the overall model column.
   inline void geostrophic_wind_forcing_indiv( core::Coupler &coupler , real dt , real lat_g , real u_g , real v_g ) {
     using yakl::c::parallel_for;
     using yakl::c::SimpleBounds;
-    auto nz      = coupler.get_nz();
-    auto ny      = coupler.get_ny();
-    auto nx      = coupler.get_nx();
-    auto ny_glob = coupler.get_ny_glob();
-    auto nx_glob = coupler.get_nx_glob();
-    auto &dm     = coupler.get_data_manager_readwrite();
-    auto uvel    = dm.get<real,3>("uvel");
-    auto vvel    = dm.get<real,3>("vvel");
-    auto imm     = dm.get<real const,3>("immersed_proportion");
-    real fcor    = 2*7.2921e-5*std::sin(lat_g/180*M_PI);
+    auto nz      = coupler.get_nz();        // number of vertical levels
+    auto ny      = coupler.get_ny();        // number of local cells in the y-direction
+    auto nx      = coupler.get_nx();        // number of local cells in the x-direction
+    auto ny_glob = coupler.get_ny_glob();   // total global number of cells in the y-direction
+    auto nx_glob = coupler.get_nx_glob();   // total global number of cells in the x-direction
+    auto &dm     = coupler.get_data_manager_readwrite();  // get the data manager with read/write access
+    auto uvel    = dm.get<real,3>("uvel");  // Get the 3D u-velocity field
+    auto vvel    = dm.get<real,3>("vvel");  // Get the 3D v-velocity field
+    auto imm     = dm.get<real const,3>("immersed_proportion");  // Get the immersed proportion field
+    real fcor    = 2*7.2921e-5*std::sin(lat_g/180*M_PI);  // Compute coriolis parameter
+    // Apply geostrophic forcing to the u and v velocity fields for each individual cell
     parallel_for( YAKL_AUTO_LABEL() , SimpleBounds<3>(nz,ny,nx) , KOKKOS_LAMBDA (int k, int j, int i) {
       uvel(k,j,i) += dt*( fcor*(vvel(k,j,i)-v_g));
       vvel(k,j,i) += dt*(-fcor*(uvel(k,j,i)-u_g));
